ee0fb6b88dd068150d85937388fe36f5611665f6
bibianAmaya <bibian.e.amaya.p@gmail.com>
2025-02-27 20:04:39 -0500

    New Definition of Tables (DDL)

--- db/001_schema.sql
+++ db/001_schema.sql
@@ -2,39 +2,90 @@
 -- SCHEMA EN LA BASE DE DATOS
 -- ==========================
 
-CREATE TABLE product_type (
-    id SERIAL PRIMARY KEY,
-    description VARCHAR(100) NOT NULL
-);
+CREATE TABLESPACE mytablespace LOCATION '/var/lib/postgresql/data';
 
-CREATE TABLE products(
-    id SERIAL PRIMARY KEY,
+-- Tabla de Categorías
+CREATE TABLE IF NOT EXISTS categories (
+    category_id BIGSERIAL PRIMARY KEY,
+    is_active BOOLEAN DEFAULT TRUE,
+    title VARCHAR(255) UNIQUE NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    image VARCHAR(255)
+) TABLESPACE mytablespace;
+
+-- Tabla de Productos
+CREATE TABLE IF NOT EXISTS products (
+    product_id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL,
-    description TEXT NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
-    product_type_id INT NOT NULL,
-    FOREIGN KEY (product_type_id) REFERENCES product_type(id) ON DELETE CASCADE
-);
+    description VARCHAR(255) NOT NULL,
+    price NUMERIC(38,2) NOT NULL,
+    category_id BIGINT NOT NULL,
+    product_status_id BIGINT NOT NULL,
+    main_image_url VARCHAR(255) NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id) ON DELETE CASCADE,
+    FOREIGN KEY (product_status_id) REFERENCES product_status(product_status_id) ON DELETE CASCADE
+) TABLESPACE mytablespace;
 
+-- Tabla de Estado de Productos
+CREATE TABLE IF NOT EXISTS product_status (
+    product_status_id BIGSERIAL PRIMARY KEY,
+    description VARCHAR(255) NOT NULL
+) TABLESPACE mytablespace;
 
-CREATE TABLE product_image (
-    id SERIAL PRIMARY KEY,
-    product_id INT NOT NULL,
-    url TEXT NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
-);
+-- Tabla de Imágenes de Productos
+CREATE TABLE IF NOT EXISTS product_images (
+    product_image_id BIGSERIAL PRIMARY KEY,
+    product_id BIGINT NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE
+) TABLESPACE mytablespace;
 
+-- Tabla de Características de Productos
+CREATE TABLE IF NOT EXISTS attributes (
+    attribute_id BIGSERIAL PRIMARY KEY,
+    is_active BOOLEAN DEFAULT TRUE,
+    icon VARCHAR(150) NOT NULL,
+    description VARCHAR(255) UNIQUE NOT NULL
+) TABLESPACE mytablespace;
 
-CREATE TABLE features(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    icon VARCHAR(100) NOT NULL
-);
+-- Relación Producto - Características (Muchos a Muchos)
+CREATE TABLE IF NOT EXISTS product_attributes (
+    product_attributes_id BIGSERIAL PRIMARY KEY,
+    product_id BIGINT NOT NULL,
+    attribute_id BIGINT NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE,
+    FOREIGN KEY (attribute_id) REFERENCES attributes(attribute_id) ON DELETE CASCADE
+) TABLESPACE mytablespace;
 
-CREATE TABLE product_features (
-    id SERIAL PRIMARY KEY,
-    product_id INT NOT NULL,
-    feature_id INT NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
-    FOREIGN KEY (feature_id) REFERENCES features(id) ON DELETE CASCADE
-);
+-- Tabla de Usuarios
+CREATE TABLE IF NOT EXISTS users (
+    user_id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(45) NOT NULL,
+    surname VARCHAR(45) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+) TABLESPACE mytablespace;
+
+-- Tabla de Roles
+CREATE TABLE IF NOT EXISTS roles (
+    role_id BIGSERIAL PRIMARY KEY,
+    role_name VARCHAR(255) UNIQUE NOT NULL
+) TABLESPACE mytablespace;
+
+-- Relación Usuarios - Roles (Muchos a Muchos)
+CREATE TABLE IF NOT EXISTS user_roles (
+    user_roles_id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    role_id BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE CASCADE
+) TABLESPACE mytablespace;
+
+-- Relación Usuarios - Productos (Usuarios que poseen productos)
+CREATE TABLE IF NOT EXISTS product_users (
+    product_users_id BIGSERIAL PRIMARY KEY,
+    product_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+) TABLESPACE mytablespace;
